36dacfcc0056ab902f11da0c32c8d65535c3a0a3
Lisa <somilisa1010@gmail.com>
2023-07-03 21:58:48 +0100

    feat: add alert, task status, clear items button

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const form = document.querySelector('.todo-form');
-// const alert = document.querySelector('.alert');
+const alert = document.querySelector('.alert');
 const todo = document.getElementById('todo');
 const submitBtn = document.querySelector('.submit-btn');
 const container = document.querySelector('.todo-container');
 const todoList = document.querySelector('.todo-list');
 const clearBtn = document.querySelector('.clear-btn');
 let posts = document.getElementById('posts');
+let data = [];
 
 let editElement;
 let editFlag = false;
@@ -19,8 +20,6 @@ form.addEventListener('submit', (e) => {
   formValidation();
 });
 
-let data = {};
-
 let acceptData = () => {
   data['text'] = todo.value;
   console.log(data);
@@ -30,35 +29,51 @@ let acceptData = () => {
 let formValidation = () => {
   if (todo.value !== '' && !editFlag) {
     console.log('success');
-    msg.innerHTML = '';
     acceptData();
   } else if (todo.value !== '' && editFlag) {
     editElement.innerHTML = todo.value;
+    displayAlert('item edited', 'success');
     setBackToDefault();
     saveData();
   } else {
-    msg.innerHTML = 'Post cannot be blank';
     console.log('failure');
+    displayAlert('please enter value', 'success');
   }
 };
 let createPost = () => {
   posts.innerHTML += `
   <div class="posts-item" >
-    <p class="title">${data.text}</p>
+    <p class="title" onClick="completedPost(this)">${data.text}</p>
     <span class="options">
       <button onClick="editPost(this)"<i  class="fas fa-edit" id="edit-btn"></i></button>
       <button onClick="deletePost(this)" id="delete-btn"><i  class="fas fa-trash-alt" ></i></button>
     </span>
+    </span>
+    <select name="status" id="">
+    // <option value="none" selected disabled hidden>Select an Option</option>
+    <option value="pending">pending</option>
+    <option value="in progress">in progress</option>
+    <option value="completed">completed</option>
+  </select>
   </div>
   `;
+  displayAlert('item added to the list', 'success');
   todo.value = '';
   saveData();
 };
 function displayAlert(text, action) {
   alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+  //remove alert
+  setTimeout(function () {
+    alert.textContent = '';
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
 }
+
 let deletePost = (e) => {
   e.parentElement.parentElement.remove();
+  displayAlert('item removed from list', 'danger');
   saveData();
 };
 let editPost = (e) => {
@@ -68,11 +83,26 @@ let editPost = (e) => {
   console.log(editFlag);
   submitBtn.textContent = 'edit';
 };
+
+function completedPost(e) {
+  e.classList.toggle('completed');
+  displayAlert('task completed', 'success');
+}
+
 function setBackToDefault() {
   todo.value = '';
   editFlag = false;
   editID = '';
   submitBtn.textContent = 'submit';
+}
+
+const items = document.querySelectorAll('.posts-item');
+function clearItems() {
+  if (items.length > 0) {
+    items.forEach((item) => {
+      list.removeChild(item);
+    });
+  }
 }
 function saveData() {
   localStorage.setItem('data', posts.innerHTML);
@@ -81,3 +111,16 @@ function showTask() {
   posts.innerHTML = localStorage.getItem('data');
 }
 showTask();
+
+function showAll() {
+  posts.innerHTML = items.filter() 
+}
+
+// let paragraphs = [...document.querySelectorAll('.title')];
+// console.log(paragraphs);
+
+// paragraphs.forEach((paragraph) => {
+//   paragraph.addEventListener('click', function () {
+//     console.log(paragraph);
+//   });
+// });
